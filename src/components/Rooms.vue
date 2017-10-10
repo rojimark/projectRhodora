2e6ba2066c349cee9d574a32c062e0b5433a3eec
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-10-10 23:36:36 +0800

    added all

---
+++ src/components/Rooms.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="rooms">
+	<div class="row">
+
+    <div class="col s12 m6">
+	<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/SampleRoom1.1.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Room 1<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Room Type 1<i class="material-icons right">close</i></span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..</p>
+    </div>
+  </div>
+</div>
+
+
+  <div class="col s12 m6">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/SampleRoom2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Room 2<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Room Type 2<i class="material-icons right">close</i></span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..</p>
+    </div>
+  </div>
+</div>
+
+
+ <div class="col s12 m6">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/SampleRoom3.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Room 3<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Room Type 3<i class="material-icons right">close</i></span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..</p>
+    </div>
+  </div>
+</div>
+
+ <div class="col s12 m6">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/SampleRoom4.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Room 4<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Room Type 4<i class="material-icons right">close</i></span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..</p>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+  
+
+
+</template>
+
+<script>
+	
+
+</script>
+
+<style scoped>
+.rooms{
+}	
+
+</style>
